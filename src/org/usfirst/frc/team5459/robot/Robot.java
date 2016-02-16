53be764da062bcc3ff78ccb2f54fd28a58971889
FilipKernan <filip.kernan@gmail.com>
2016-02-15 23:08:08 -0500

    first commit

--- src/org/usfirst/frc/team5459/robot/Robot.java
+++ src/org/usfirst/frc/team5459/robot/Robot.java
@@ -3,14 +3,8 @@ package org.usfirst.frc.team5459.robot;
 
 import java.security.PublicKey;
 
-import edu.wpi.first.wpilibj.SensorBase;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.Ultrasonic;
-import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 
 
 /**
@@ -21,23 +15,19 @@ import edu.wpi.first.wpilibj.*;
  * directory.
  */
 public class Robot extends IterativeRobot {
-    RobotDrive Rook;//drive name
-    Joystick Stick1;
-    Joystick Stick2;//the joysticks
-    Victor Arm;//can talon
-    Victor Shoot1;
-    Victor Shoot2;//victors for shooter
-    Victor Treads;//victor for treads
-    Servo Gate;//servo for gate
-    Servo Push;//servo for pushing out ball
-    ADXRS450_Gyro Gyro;//gyro
-    Ultrasonic ForwardSensor, SideSensor;
-    double speedX, speedY, speedRote, gyroAngle;
-    double Kp = 0.03;
-    boolean armed = false;
-    boolean hasShot = false;
-    int tickCount = 0;
-    boolean countTick = false;
+    RobotDrive rook;//drive name
+    Joystick stick1, stick2;//the joysticks
+    Victor arm,shoot1,shoot2,treads;//victor controllers
+    Talon leftRear;
+    Servo gate,push;//servos for pushing out ball and gate
+    ADXRS450_Gyro gyro;//gyro
+    Ultrasonic forwardSensor, sideSensor;
+    CameraServer camera;
+    double speedX, speedY, speedRote, gyroAngle, throttle;
+    //double Kp = 0.03;
+    boolean armed = false,hasShot = false,countTick = false, xPosition, yPosition;
+    int tickCount = 0, xDistance, yDistance, currentTick = 0;
+    
     
     
 	/**
@@ -45,92 +35,121 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-    	Rook = new RobotDrive(4,7, 0, 2);
-    	Stick1 = new Joystick(1);
-    	Stick2 = new Joystick(2);
-    	Shoot1 = new Victor(3);
-    	Shoot2 = new Victor(5);
-    	Treads = new Victor(1);
-    	Arm = new Victor(6);
-    	Gate = new Servo(8);
-    	Push = new Servo(9);
-    	Gyro = new ADXRS450_Gyro();
-    	Gyro.calibrate();
-    	Gyro.reset();
-    	
+    	rook = new RobotDrive(4,7, 0, 2);
+    	rook.setInvertedMotor(MotorType.kRearLeft, true);
+    	rook.setInvertedMotor(MotorType.kFrontLeft,  true);
+    	rook.setSafetyEnabled(true);
+    	rook.setExpiration(0.1);
+    	stick1 = new Joystick(1);
+    	stick2 = new Joystick(2);
+    	shoot1 = new Victor(3);
+    	shoot2 = new Victor(5);
+    	treads = new Victor(1);
+    	arm = new Victor(6);
+    	arm.setSafetyEnabled(true);
+    	arm.setExpiration(0.1);
+    	gate = new Servo(8);
+    	push = new Servo(9);
+    	gyro = new ADXRS450_Gyro();
+    	gyro.calibrate();
+    	gyro.reset();
+    	camera = CameraServer.getInstance();
+    	camera.setQuality(50);
+    	camera.startAutomaticCapture("cam0 ");
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-    	//TODO write auto
-    	//TODO write auto
-    	//TODO write auto
-    	//TODO write auto
+    	gyro.reset();
+    	if (tickCount< 50) {
+			rook.mecanumDrive_Polar(0.6, 0, 0);//drives forward 
+		}
+    	if (tickCount > 50 && xDistance > 4308) {
+			rook.mecanumDrive_Cartesian(0.5, 0, 0, gyroAngle);
+		}
+    	if(xDistance <= 4308 ){
+    		xPosition = true;
+    	}
+    	if (tickCount > 50 && yDistance > 914) {
+			rook.mecanumDrive_Cartesian(0, 0.5, 0, gyroAngle);
+		}
+    	if (yDistance <= 914 ) {
+			yPosition = true;
+		} 
+    	if (xPosition && yPosition) {
+			
+			currentTick = tickCount;
+			if (tickCount >= currentTick && tickCount <= currentTick + 7) {
+				rook.mecanumDrive_Polar(0.65, 60, 0.75);;
+			}
+		}
+    	//TODO add shoot
+    	// code for arduino & ultrasonic
+    	tickCount++;
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-    	
-    	speedX = Stick1.getX() * Stick1.getThrottle();
-    	speedY = Stick1.getY() * Stick1.getThrottle();
-    	speedRote = Stick1.getDirectionDegrees() * Stick1.getThrottle();
-    	gyroAngle = Gyro.getAngle();
+    	throttle = (stick1.getThrottle()/2);
+    	speedX = stick1.getX() * throttleEncode(stick1);
+    	speedY = stick1.getY() * throttleEncode(stick1);
+    	speedRote = stick1.getDirectionDegrees() * throttleEncode(stick1);
+    	gyroAngle = gyro.getAngle();
     	/*if (gyroAngle >= 360) {
 			gyroAngle = gyroAngle - 360;
 		}*/
-    	if (Stick1.getRawButton(2)) {
-			Rook.mecanumDrive_Cartesian(speedX, speedY, speedRote, -gyroAngle * Kp);//if angle starts freaking out then uncomment the above if statment 
+    	if (stick1.getRawButton(2)) {
+			rook.mecanumDrive_Cartesian(speedX, speedY, speedRote, gyroAngle /* Kp*/);//if angle starts freaking out then uncomment the above if statment 
     	}else {
-    		Rook.mecanumDrive_Cartesian(speedX, speedY, 0, -Gyro.getAngle() * Kp);
+    		rook.mecanumDrive_Cartesian(speedX, speedY, 0, gyroAngle/* Kp*/);
 		}
-		if(Stick1.getRawButton(1)){
-    		Treads.set(1.0);
+		if(stick1.getRawButton(1)){
+    		treads.set(1.0);
     	}//activate treads
-    	if(Stick2.getRawButton(2)){//arm shooter
-    		Shoot1.set(1.0);
-    		Shoot2.set(-1.0);
+    	if(stick2.getRawButton(2)){//arm shooter
+    		shoot1.set(1.0);
+    		shoot2.set(-1.0);
     		armed = true;//will only fire if armed is true
     	}else {
-			armed = false;
+			armed = false; 
 		}
-    	if (Stick2.getRawButton(1) && armed == true) {//shoots
-			Gate.set(1.0);
-			Push.set(1.0); 
+    	if (stick2.getRawButton(1) && armed == true) {//shoots
+			gate.set(1.0);
+			push.set(1.0); 
 			hasShot = true;
 			countTick = true;
 		}
     	
     	if (hasShot == true && tickCount == 2){//auto reset push
-    		Push.set(0.0);
+    		push.set(0.0);
     		countTick = false;
+    		tickCount = 0;
     	}
-    	if (Stick2.getRawButton(12) && hasShot == true) {//closes gate
-			Gate.set(0.0);
+    	if (stick2.getRawButton(12) && hasShot == true) {//closes gate
+			gate.set(0.0);
 		}
-    	/*TODO figure out order of operations
+    	//TODO figure out order of operations
     	   
-    	  TODO camera code 
-    	*/
-    	
-    	if (Stick2.getRawButton(6)) {
-    		Shoot1.set(-1.0);
-    		Shoot2.set(1.0);
+    	if (stick2.getRawButton(6)) {
+    		shoot1.set(-1.0);
+    		shoot2.set(1.0);
 		}//draws in ball
     	
-    	if (Stick2.getRawButton(5)) {//arm up
-			Arm.set(0.5);
+    	if (stick2.getRawButton(5)) {//arm up
+			arm.set(0.2);
 		}
-    	if (Stick2.getRawButton(3)) {//arm down
-			Arm.set(-0.5);
+    	if (stick2.getRawButton(3)) {//arm down
+			arm.set(-0.2);
 		}
     	
     	if (countTick) {
 			tickCount++;
 		}//counts ticks
+    	Timer.delay(0.005);
     	//TODO: rerun auto on stick 2
     }
     
@@ -140,5 +159,28 @@ public class Robot extends IterativeRobot {
     public void testPeriodic() {
     	//TODO write test
     }
-        
+    
+    /*
+     * 
+     *
+     */
+    public double throttleEncode(Joystick stick) {
+		double[] encode ={1.0,0.825,0.65,0.475,0.3, 0.125};//values for var speed
+    	if (stick.getThrottle()>= 0.6) {//TODO add code for 6th speed
+			return encode[0];
+		}
+		if (stick.getThrottle()<= -0.6) {
+			return encode[4];
+		}
+    	if (stick.getThrottle() < 0.6 && stick.getThrottle() >= 0.2) {
+			return encode[1];
+		}
+    	if (stick.getThrottle() < -0.6 && stick.getThrottle() >= -0.2) {
+			return encode[3];
+		}
+    	if (stick.getThrottle() < 0.2 && stick.getThrottle() > -0.2) {
+			return encode[2];
+		}
+    	return 0.0;
+	}
 }
